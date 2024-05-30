cdda3f529de228fbdec8f77613ff7123436d8b59
Nithin Peddapelly <peddapally.nithin777@gmail.com>
2024-05-30 20:12:39 +0530

    Add files via upload

---
+++ a2.java
@@ -0,0 +1,9 @@
+public class a2 {
+    public static void main(String args[])
+    {
+        char a ='a';
+        char b = 'b';
+        System.out.println((int)a);
+        System.out.println(b-a);
+    }
+}
