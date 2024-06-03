0563448f793b9613f62e99ac4e05395eaffb8b73
Nithin Peddapelly <peddapally.nithin777@gmail.com>
2024-06-03 20:30:33 +0530

    Add files via upload

--- j10.java
+++ j10.java
@@ -3,8 +3,10 @@ public class j10 {
     public static void main(String args[]){
         Scanner scn = new Scanner(System.in);
         int a = scn.nextInt();
+        scn.close();
         String b = (a < 0)? "Negative" : "Positive";
         System.out.println(b);
+        
         
 
     }
