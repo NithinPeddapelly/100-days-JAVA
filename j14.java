31e1a263064e65034f69372ba14fcf1cebb7e79e
Nithin Peddapelly <peddapally.nithin777@gmail.com>
2024-06-03 20:31:17 +0530

    Add files via upload

---
+++ j14.java
@@ -0,0 +1,12 @@
+public class j14 {
+    public static void main(String srgs[]){
+        int a = 123456;
+        while(a > 0){
+            int b  = a % 10;
+            System.out.print(b);
+            a = a/10;
+        }
+        
+    }
+}
+ 
