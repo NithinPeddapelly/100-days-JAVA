6a832586d2507cbe84e528bccaf13aad6c66734d
NithinPeddapelly <peddapally.nithin777@gmail.com>
2025-02-14 01:14:19 +0530

    Refactor counting sort implementation for clarity and efficiency

--- j57_CountingSort.java
+++ j57_CountingSort.java
@@ -1,34 +1,34 @@
 public class j57_CountingSort {
     public static void main(String args[]) {
-        int arr[] = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 0, 1, 2, 3, 4 };
+        int newarr[] = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 0, 1, 2, 3, 4 };
         int largest = Integer.MIN_VALUE;
-        for (int i = 0; i < arr.length; i++) {
-            largest = Math.max(largest, arr[i]);
+        for (int i = 0; i < newarr.length; i++) {
+            largest = Math.max(largest, newarr[i]);
         }
 
-        int newarr[] = new int[largest + 1];
-        for (int i = 0; i < arr.length; i++) {
-            int a = arr[i];
-            newarr[a]++;
+        int newnewarr[] = new int[largest + 1];
+        for (int i = 0; i < newarr.length; i++) {
+            int a = newarr[i];
+            newnewarr[a]++;
 
         }
         int c = 0;
-        for (int j = 0; j <= largest; j++) {        // MINE
-            int b = newarr[j];
-            for (int k = 0; k < b; k++) {
-                arr[c] = j;
-                c++;
-            }  
-        }
-        // for(int j = 0;j< newarr.length;j++){        // APNA COLLEGE
-        //     while(newarr[j] > 0){
-        //         arr[c] = j;
+        // for (int j = 0; j <= largest; j++) {        // MINE
+        //     int b = newnewarr[j];
+        //     for (int k = 0; k < b; k++) {
+        //         newarr[c] = j;
         //         c++;
-        //         newarr[j]
-        //     }
+        //     }  
         // }
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
+        for(int j = 0;j< newnewarr.length;j++){        // APNA COLLEGE
+            while(newnewarr[j] > 0){
+                newarr[c] = j;
+                c++;
+                newnewarr[j]--;
+            }
+        }
+        for (int i = 0; i < newarr.length; i++) {
+            System.out.print(newarr[i] + " ");
         }
     }
 }
