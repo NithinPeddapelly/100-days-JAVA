6c3a30941a8adf4ff3dc298cadd08a16620533d8
NithinPeddapelly <peddapally.nithin777@gmail.com>
2025-02-15 17:26:28 +0530

    new file overloading and overriding

---
+++ j76_Over_Loading_Over_Riding.java
@@ -0,0 +1,35 @@
+public class j76_Over_Loading_Over_Riding {
+    public static void main(String args[]){
+        Deer golden_deer = new Deer();
+        golden_deer.eats();
+
+        Caluclator ne = new Caluclator();
+        System.out.println(ne.sum( 12, 6));
+        System.out.println(ne.sum( 12.5F, 6.3F));
+
+    }
+}
+
+class Caluclator{
+    int sum(int a,int b){
+        return a+b;
+    }
+    int sum(int a, int b,int c){ // Over loading the sum() method of Caluclator class
+        return a+b+c;
+    }
+    float sum(float a, float b){ // Over loading the sum() method of Caluclator class
+        return a+b;
+    }
+}
+
+
+class Animal{
+    void eats() {
+        System.out.println("Animal eats anyting"); // initial eats() method of Animal class
+    }
+}
+class Deer extends Animal{
+    void eats(){
+        System.out.println("Deer eats grass");  // Over Riding the eats() method of Animal class
+    }
+}
