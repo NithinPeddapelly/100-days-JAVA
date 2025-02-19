edf4ed295099d4163e28f105315140396470562f
NithinPeddapelly <peddapally.nithin777@gmail.com>
2025-02-19 18:08:27 +0530

    Updated 03

--- j77_abstract_class.java
+++ j77_abstract_class.java
@@ -1,19 +1,27 @@
 public class j77_abstract_class {
     public static void main (String args[]){
     
-        Horse H = new Horse();
-        H.eats();
-        H.walk();
-        Chicken c = new Chicken();
-        c.eats();
-        c.walk();
+        // Horse H = new Horse();
+        // H.eats();
+        // H.walk();
+        // Chicken c = new Chicken();
+        // c.eats();
+        // c.walk();
+        // System.out.println(H.color);
+        // System.out.println(c.color);
+        // c.changeColor();
+        // H.changeColor();
         
+        Mustang My = new Mustang(); // Animal-> Horse-> Mustang 
+        // My.eats();
+        // My.walk();
     }
 }
 abstract class Animal{  // abstract class - can have abstract methods - cannot create class objects - can create constructor
     String color;
     Animal(){
-        color = "Brown";
+        System.out.println("Animal constructor called");
+        color = "Brown"; //By default it makes the color of the animal brown until we change it using changeColor method.
     }
     void eats(){
         System.out.println("eats");
@@ -22,17 +30,30 @@ abstract class Animal{  // abstract class - can have abstract methods - cannot c
 }
 
 class Horse extends Animal{
+    Horse(){
+        System.out.println("Horse constructor called");
+    }
     void changeColor(){
-        color = "White";
+        color = "White"; //
+        System.out.println(color);
     }
     void walk(){
         System.out.println("Horse walks on 4 legs");
     }
 }
 
+class Mustang extends Horse{
+    Mustang(){
+        System.out.println("Mustang constructor called");
+    }
+    void walk(){
+        System.out.println("Walks on 4 legs");
+    }
+}
 class Chicken extends Animal{
     void changeColor(){
         color = "orange";
+        System.out.println(color);
     }
     void walk(){
         System.out.println("Chicken walks on 2 legs");
